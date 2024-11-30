73305636b2863c50cef28becc52a6739b878376e
Cristian Fernando <cristianfernandovilluendas@gmail.com>
2024-11-30 22:55:11 +0100

    cambios css homeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,31 @@
 <template>
-  <header><h1>HoraFlox</h1></header>
-
-  <main>
-    <p>Bienvenid@s a la mejor aplicación de registros de horas extras. Registra y consulta tus horas rápidamente.</p>
-    <nav>
-      <button>Registrar horas</button>
-      <button>Ver registro de horas</button>
-    </nav>
-  </main>
-  <footer>
-    <p>© 2024 Cristian, Todos los derechos reservados.</p>
-  </footer>
+  <header>
+    <h1>HoraFlox</h1>
+  </header>
+  <div class="contenedor">
+    <main>
+      <p>Bienvenid@s a la mejor aplicación de registros de horas extras. Registra y consulta tus horas rápidamente.</p>
+      <nav>
+        <button>Registrar horas</button>
+        <button>Ver registro de horas</button>
+      </nav>
+    </main>
+    <footer>
+      <p>© 2024 Cristian, Todos los derechos reservados.</p>
+    </footer>
+  </div>
 </template>
 
 <script setup>
 
 </script>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  display: flex;
+  justify-content: center;
+  margin-top: 80px;
+  font-size: 4rem;
+  font-weight: 400;
+}
+</style>
