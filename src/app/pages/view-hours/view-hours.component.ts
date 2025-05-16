0d33231888eb786b7dda2b4e7c57e503e234ff49
Cristian Fernando <cristianfernandovilluendas@gmail.com>
2025-05-16 08:59:00 +0200

    Creamos nueva pagina de ver horas y lo cargamos en el enrutado

---
+++ src/app/pages/view-hours/view-hours.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-hours',
+  imports: [],
+  templateUrl: './view-hours.component.html',
+  styleUrl: './view-hours.component.css'
+})
+export class ViewHoursComponent {
+
+}
