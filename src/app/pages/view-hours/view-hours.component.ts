4d5aa611a12311fc80abb7f23ff55d6fe01fa092
Cristian Fernando <cristianfernandovilluendas@gmail.com>
2025-05-21 17:46:40 +0200

    terminamos view home

--- src/app/pages/view-hours/view-hours.component.ts
+++ src/app/pages/view-hours/view-hours.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-view-hours',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './view-hours.component.html',
   styleUrl: './view-hours.component.css'
 })
