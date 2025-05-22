1563d843c18b180988144da8ca40b69bf6b301c4
Cristian Fernando <cristianfernandovilluendas@gmail.com>
2025-05-22 10:59:31 +0200

    creamos pagina de registrar horas y lo cargamos en rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ViewHoursComponent } from './pages/view-hours/view-hours.component';
+import { RecordHoursComponent } from './pages/record-hours/record-hours.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +16,9 @@ export const routes: Routes = [
       {
         path: 'inicio/ver-horas',
         component: ViewHoursComponent,
+      },
+      {
+        path: 'inicio/registrar-horas',
+        component: RecordHoursComponent,
       },
 ];
---
+++ src/app/pages/record-hours/record-hours.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-record-hours',
+  imports: [],
+  templateUrl: './record-hours.component.html',
+  styleUrl: './record-hours.component.css'
+})
+export class RecordHoursComponent {
+
+}
