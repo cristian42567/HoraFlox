0ca42fad645d782c72ed3487c5988541991ae43f
Cristian Fernando <cristianfernandovilluendas@gmail.com>
2025-06-04 18:29:05 +0200

    creamos notfound

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ViewHoursComponent } from './pages/view-hours/view-hours.component';
 import { RecordHoursComponent } from './pages/record-hours/record-hours.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
     {
@@ -20,5 +21,9 @@ export const routes: Routes = [
       {
         path: 'inicio/registrar-horas',
         component: RecordHoursComponent,
+      },
+      {
+        path: '**',
+        component: NotFoundComponent,
       },
 ];
